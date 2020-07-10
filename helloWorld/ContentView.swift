3240135f878169f3bc1a995ac8c99ac324cfa752
Mehmet Aydin Bekem <bekemaydin@gmail.com>
2020-07-10 21:51:37 +0300

    Created project

---
+++ helloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  helloWorld
+//
+//  Created by Mehmet Aydın Bekem on 10.07.2020.
+//  Copyright © 2020 Mehmet Aydın Bekem. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
